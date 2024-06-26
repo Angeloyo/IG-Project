d779a23f56ff0ec40912bbca241d9d55e27cfd82
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2024-06-26 19:27:27 +0200

    updating

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
+const gui = new dat.GUI();
+gui.width = 360;
+
+let continuePhysics = true; 
+// let targetCameraPosition = new THREE.Vector3(0, 0, 0);
+let timeStep = 0.0001;
+// let defaultTrailLimit = 800;
+let defaultTrailLimit = 500;
+let G = 6.67430e-11 * 1e-10;  
+let focusedBody = null;
+let velocityRangeStart = 7000;
+let velocityRangeEnd = 15500;
+let positionRangeStart = 0;
+let positionRangeEnd = 400;
 
 // Scene setup
 const scene = new THREE.Scene();
@@ -7,7 +21,7 @@ scene.background = new THREE.Color(0x000000);
 
 // Camera setup
 const camera = new THREE.PerspectiveCamera
-(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+(75, window.innerWidth / window.innerHeight, 0.1, 12000);
 camera.fov = 75;
 camera.position.set(0, 0, 100);
 camera.lookAt(new THREE.Vector3(0, 0, 0)); // Look at the origin
@@ -23,7 +37,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.minDistance = 0;
-controls.maxDistance = 450;
+controls.maxDistance = 750;
 controls.target.set(0, 0, -200);
 controls.update();
 
@@ -37,7 +51,7 @@ const loader = new THREE.TextureLoader();
 const bgTexture = loader.load('/src/textures/universe2.jpg');
 
 // Sphere setup
-const bgGeometry = new THREE.SphereGeometry(1000, 60, 40);
+const bgGeometry = new THREE.SphereGeometry(5000, 60, 40);
 const bgMaterial = new THREE.MeshBasicMaterial({
     map: bgTexture,
     side: THREE.BackSide
@@ -69,43 +83,78 @@ function getRandomVector3InRange(min, max) {
 const celestialBodies = [
     {
         type: 'star',
-        mass: 1.989e30,
-        position: getRandomVector3InRange(0, 400),
-        velocity: getRandomVector3InRange(1000, 10500),
+        focus: false,
+        mass: 2e30,
+
+        initialPosition: null,
+        randomInitialPosition: true,
+        position: null,
+        
+        initialVelocity: null,
+        randomInitialVelocity: true,
+        velocity: null,
+        
         mesh: null,
         light: null,
-        trailPositions: null,
-        trailLimit: null,
+        trailPositions: [],
+        trailLimit: defaultTrailLimit,
+        trailLine: null,
     },
     {
         type: 'star',
-        mass: 5.972e24,
-        position: getRandomVector3InRange(0, 400),
-        velocity: getRandomVector3InRange(1000, 10500),
+        focus: false,
+        mass: 9e30,
+
+        initialPosition: null,
+        randomInitialPosition: true,
+        position: null,
+        
+        initialVelocity: null,
+        randomInitialVelocity: true,
+        velocity: null,
+        
         mesh: null,
         light: null,
-        trailPositions: null,
-        trailLimit: null,
+        trailPositions: [],
+        trailLimit: defaultTrailLimit,
+        trailLine: null,
     },
     {
         type: 'star',
-        mass: 5.972e28,
-        position: getRandomVector3InRange(0, 400),
-        velocity: getRandomVector3InRange(1000, 10500),
+        focus: false,
+        mass: 5e30,
+
+        initialPosition: null,
+        randomInitialPosition: true,
+        position: null,
+        
+        initialVelocity: null,
+        randomInitialVelocity: true,
+        velocity: null,
+        
         mesh: null,
         light: null,
-        trailPositions: null,
-        trailLimit: null,
+        trailPositions: [],
+        trailLimit: defaultTrailLimit,
+        trailLine: null,
     },
     {
         type: 'planet',
-        mass: 5.972e18,
-        position: getRandomVector3InRange(0, 400),
-        velocity: getRandomVector3InRange(1000, 10500),
+        focus: false,
+        mass: 5e18,
+        
+        initialPosition: null,
+        randomInitialPosition: true,
+        position: null,
+
+        initialVelocity: null,
+        randomInitialVelocity: true,
+        velocity: null,
         mesh: null,
         light: null,
-        trailPositions: null,
-        trailLimit: null,
+        trailPositions: [],
+        trailLimit: defaultTrailLimit,
+        trailLine: null,
     },
 ];
 
@@ -125,6 +174,12 @@ const planetMaterial = new THREE.MeshBasicMaterial({
 celestialBodies.forEach(body => {
     // const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 
+    body.initialPosition = getRandomVector3InRange(positionRangeStart, positionRangeEnd);
+    body.initialVelocity = getRandomVector3InRange(velocityRangeStart, velocityRangeEnd);
+
+    body.velocity = body.initialVelocity.clone();
+    body.position = body.initialPosition.clone();
+
     let bodyGeometry = body.type === 'star' ? starGeometry : planetGeometry;
 
     let bodyMaterial = body.type === 'star' ? starMaterial : planetMaterial;
@@ -140,8 +195,7 @@ celestialBodies.forEach(body => {
     scene.add(body.mesh);
     scene.add(body.light);
 
-    body.trailPositions = []; // store positions for trail
-    body.trailLimit = 5000; // limit the number of points in the trail
+
 });
 
 function calculateGeometricCenter() {
@@ -156,14 +210,9 @@ function calculateGeometricCenter() {
     return sumPosition;
 }
 
-let continuePhysics = true; 
-
-let timeStep = 0.0001;
-
 function updatePhysics() {
     if (!continuePhysics) return;  // stop physics if collision detected
 
-    const G = 6.67430e-11 * 1e-10   ;  // Gravitational constant (scaled down)
     let force, distanceVector, distance, forceMagnitude;
 
     celestialBodies.forEach(body => {
@@ -204,19 +253,54 @@ function updatePhysics() {
         body.mesh.position.copy(body.position);
         body.light.position.copy(body.position);
     });
-
 }
 
-function createTrailGeometry() {
-    celestialBodies.forEach(body => {
-        if (body.trail) {
-            scene.remove(body.trail); // remove the old trail
+function handleFocusChange(bodyToFocus) {
+
+    if (isHandlingFocusChange) return;
+
+    if (bodyToFocus === null) {
+        focusedBody = null;
+        updateCameraFocus();
+        return;
+    }
+
+    isHandlingFocusChange = true;
+
+    console.log("Focus changed to: ", bodyToFocus);
+    for (let i = 0; i < celestialBodies.length; i++) {
+        const body = celestialBodies[i];
+        if (body !== bodyToFocus) {
+            body.focus = false;
+            focusedBody = null;
+            controllers.focusControllers[i].setValue(false).updateDisplay();
         }
-        const material = new THREE.LineBasicMaterial({ color: 0xffffff });
-        const geometry = new THREE.BufferGeometry().setFromPoints(body.trailPositions);
-        body.trail = new THREE.Line(geometry, material);
-        scene.add(body.trail);
-    });
+        else {
+            body.focus = true;
+            focusedBody = body;
+            controllers.focusControllers[i].setValue(true).updateDisplay();
+            for (let j = i + 1; j < celestialBodies.length; j++) {
+                controllers.focusControllers[j].setValue(false).updateDisplay();
+                celestialBodies[j].focus = false;
+            }
+            break;
+        }
+    }
+    updateCameraFocus();
+    isHandlingFocusChange = false;
+}
+
+let isHandlingFocusChange = false; // Flag to prevent recursive calls
+
+function updateCameraFocus() {
+    if (focusedBody !== null) {
+        bgSphere.position.copy(focusedBody.position);
+        controls.target.set(focusedBody.position.x, focusedBody.position.y, focusedBody.position.z);
+    } else {
+        const geometricCenter = calculateGeometricCenter();
+        bgSphere.position.copy(geometricCenter);
+        controls.target.set(geometricCenter.x, geometricCenter.y, geometricCenter.z);
+    }
 }
 
 function animate() {
@@ -227,52 +311,166 @@ function animate() {
 
     createTrailGeometry();
 
-    const geometricCenter = calculateGeometricCenter();
-
-    // // Adjust camera position
-    // camera.position.x = geometricCenter.x + 200;
-    // camera.position.y = geometricCenter.y + 200;
-    // camera.position.z = geometricCenter.z + 200;
-    // camera.lookAt(geometricCenter);
-
-    bgSphere.position.copy(geometricCenter);
-
-    controls.target.set(geometricCenter.x, geometricCenter.y, geometricCenter.z);
+    updateCameraFocus();
 
     controls.update();
     
     renderer.render(scene, camera);
 }
 
-animate();
+function startSimulation() {
+    continuePhysics = true;
+}
 
-const gui = new dat.GUI();
-
-const settings = {
-    Start: () => startSimulation(),
-    Pause: () => pauseSimulation(),
-    TimeSpeed: timeStep,
-};
-
-gui.add(settings, 'Pause');
 function pauseSimulation() {
     continuePhysics = false;
 }
 
-gui.add(settings, 'Start');
-function startSimulation() {
+function restartSimulation() {
     continuePhysics = true;
-    // animate();
+    celestialBodies.forEach(body => {
+
+        if (body.randomInitialPosition) {
+            body.initialPosition = getRandomVector3InRange(positionRangeStart, positionRangeEnd);
+        } 
+
+        if (body.randomInitialVelocity) {
+            body.initialVelocity = getRandomVector3InRange(velocityRangeStart, velocityRangeEnd);
+        }
+
+        body.position = body.initialPosition.clone();
+        body.velocity = body.initialVelocity.clone();
+
+        controllers.velocityControllers.forEach((ctrlGroup, index) => {
+            if (celestialBodies[index]) {
+                ctrlGroup.vx.setValue(celestialBodies[index].initialVelocity.x).updateDisplay();
+                ctrlGroup.vy.setValue(celestialBodies[index].initialVelocity.y).updateDisplay();
+                ctrlGroup.vz.setValue(celestialBodies[index].initialVelocity.z).updateDisplay();
+            }
+        });
+    
+        controllers.positionControllers.forEach((ctrlGroup, index) => {
+            if (celestialBodies[index]) {
+                ctrlGroup.px.setValue(celestialBodies[index].position.x).updateDisplay();
+                ctrlGroup.py.setValue(celestialBodies[index].position.y).updateDisplay();
+                ctrlGroup.pz.setValue(celestialBodies[index].position.z).updateDisplay();
+            }
+        });
+
+        body.mesh.position.copy(body.position);
+        body.light.position.copy(body.position);
+        body.trailPositions = [];
+    });
 }
 
-gui.add(settings, 'TimeSpeed', 0.000005, 0.0003).name('Time Speed').onChange(value => {
+function updateAllTrails() {
+    celestialBodies.forEach(body => {
+        while (body.trailPositions.length > body.trailLimit) {
+            body.trailPositions.shift();
+        }
+        createTrailGeometry();
+    });
+}
+
+function createTrailGeometry() {
+    celestialBodies.forEach(body => {
+        if (body.trailLine) {
+            scene.remove(body.trailLine);
+        }
+        const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+        const geometry = new THREE.BufferGeometry().setFromPoints(body.trailPositions);
+        body.trailLine = new THREE.Line(geometry, material);
+        scene.add(body.trailLine);
+    });
+}
+
+const simulationControls = gui.addFolder('Simulation Controls');
+
+simulationControls.add({startSimulation}, 'startSimulation').name("Start Simulation");
+simulationControls.add({pauseSimulation}, 'pauseSimulation').name("Pause Simulation");
+simulationControls.add({restartSimulation}, 'restartSimulation').name("Restart Simulation");
+simulationControls.add({timeStep}, 'timeStep', 0.000005, 0.0003).name('Time Speed').onChange(value => {
     timeStep = value;
     console.log("New timeStep: ", timeStep);
 });
+simulationControls.add({G}, 'G', 6.67430e-11 * 1e-11, 6.67430e-11 ).name('Gravitational Constant');
+simulationControls.open();
+
+const celestialBodySettings = gui.addFolder('Celestial Body Settings');
+
+celestialBodySettings.add({defaultTrailLimit}, 'defaultTrailLimit', 0, 1500).name('Trail limit').onChange(value => {
+    defaultTrailLimit = value;
+    // createTrailGeometry();
+    celestialBodies.forEach(body => {
+        body.trailLimit = value; // Update trail limit for each body
+    });
+    updateAllTrails();
+    console.log("New trail limit: ", defaultTrailLimit);
+});
 
 
+let controllers = {
+    velocityControllers: [],
+    positionControllers: [],
+    focusControllers: []
+};
+
+celestialBodies.forEach((body, index) => {
+    const bodyFolder = celestialBodySettings.addFolder(`Body ${index + 1} - ${body.type}`);
+    
+    bodyFolder.add(body, 'mass', 1e15, 1e31).name('Mass');
+
+    let vx = bodyFolder.add(body.initialVelocity, 'x', velocityRangeStart, velocityRangeEnd).name('Initial velocity (x)');
+    let vy = bodyFolder.add(body.initialVelocity, 'y', velocityRangeStart, velocityRangeEnd).name('Initial velocity (y)');
+    let vz = bodyFolder.add(body.initialVelocity, 'z', velocityRangeStart, velocityRangeEnd).name('Initial velocity (z)');
+
+    vx.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+    vy.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+    vz.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+
+    bodyFolder.add(body, 'randomInitialVelocity').name('Random initial velocities').onChange(value => {
+        if (value) {
+            vx.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+            vy.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+            vz.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+        } else {
+            vx.__li.style = "";
+            vy.__li.style = "";
+            vz.__li.style = "";
+        }
+    });
+
+    controllers.velocityControllers.push({ vx, vy, vz });
+
+    let px = bodyFolder.add(body.initialPosition, 'x', positionRangeStart, positionRangeEnd).name('Initial position (x)');
+    let py = bodyFolder.add(body.initialPosition, 'y', positionRangeStart, positionRangeEnd).name('Initial position (y)');
+    let pz = bodyFolder.add(body.initialPosition, 'z', positionRangeStart, positionRangeEnd).name('Initial position (z)');
+
+    px.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+    py.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+    pz.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+    
+    bodyFolder.add(body, 'randomInitialPosition').name('Random initial position').onChange(value => {
+        if (value) {
+            px.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+            py.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+            pz.__li.style = "opacity: 0.5; filter: grayscale(100%) blur(0.5px); pointer-events: none;";
+        } else {
+            px.__li.style = "";
+            py.__li.style = "";
+            pz.__li.style = "";
+        }
+    });
+
+    controllers.positionControllers.push({ px, py, pz });
+
+    let focusC = bodyFolder.add(body, 'focus').name('Focus').onChange(() => handleFocusChange(body));
+    
+    controllers.focusControllers.push(focusC);
+
+    bodyFolder.open();
+});
+celestialBodySettings.open(); 
 
 
-
-
-
+animate();
